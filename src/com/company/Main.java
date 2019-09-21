01e98520fe2a80537346652251f09c0a420a9929
GGabi <gab.gel85@yahoo.com>
2019-09-21 21:23:38 +0200

    base architect

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] array = new int[][]{{-3, -3, -3, 4}, {-3, -3, 4, 4}, {-3, 3, 3, 3}, {-2, 1, 2, 3}};
+        int[][] array = new int[][]{{1, 2, 3, 4}, {2, 3, 4, 4}, {3, 3, 3, 3}, {2, 1, 2, 3}};
 
 
         System.out.println("1st step");
@@ -18,73 +18,6 @@ public class Main {
         System.out.println("2nd step");
 
 
-        int[] coordinates = {3, 1};
-        int waterDepth = -3;
-        int min = 1;
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if ((i <= coordinates[0] && j <= coordinates[1]) && (array[i][j] <= min) || array[i][j] < 0) {
-                    if (i == array.length - 1 && j < array.length - 1) {
-                        if (array[i][j + 1] > min && array[i][j + 1] < 0) {
-                            array[i][j + 1]--;
-                        } else if (array[i][j + 1] > 0 && Math.abs(array[i][j]) > array[i][j + 1] && array[i][j] < 0) {
-                            array[i][j + 1] *= (-1);
-
-                        }
-                        else if (Math.abs(array[i][j]) < array[i][j + 1] || Math.abs(array[i][j]) < min) {
-                            array[i][j]--;
-                        } else if (array[i][j] == min && array[i][j] < 0) {
-                            array[i][j]--;
-                        }else if(array[i][j] > 0 && array[i][j] > array[i][j + 1]){
-                            array[i][j] *= (-1);
-                        }
-                    } else if (j == array.length - 1 && i < array.length - 1) {
-                        if (array[i + 1][j] > min && array[i + 1][j] < 0) {
-                            array[i + 1][j]--;
-                        } else if (array[i + 1][j] > 0 && j < array.length - 1) {
-                            array[i + 1][j] *= (-1);
-                        } else if (Math.abs(array[i][j]) < array[i + 1][j]) {
-                            array[i][j]--;
-                        } else if (array[i][j] > 0 && array[i + 1][j] < Math.abs(min) && array[i][j] < min) {
-                            array[i][j] *= (-1);
-                        } else if (Math.abs(array[i][j]) < min && array[i][j] < 0) {
-                            array[i][j]--;
-                        }else if(array[i][j] == Math.abs(min) && array[i][j] > 0){
-                            array[i][j] *= (-1);
-                        }
-                    } else if ((i < array.length - 1 && j < array.length - 1) || (i == array.length - 1 && j == array.length - 1)) {
-
-
-                        if (Math.abs(min) > Math.abs(waterDepth) && min < 0) {
-                            array[i][j]--;
-
-                        } else if (waterDepth != array[i][j] && min > 0 && array[i][j] > 0 && Math.abs(waterDepth) >= min) {
-                            array[i][j] *= (-1);
-
-                        } else if (waterDepth == array[i][j] && min > Math.abs(array[i][j])) {
-                            array[i][j]--;
-                        } else if (Math.abs(min) < Math.abs(waterDepth) && array[i][j] != waterDepth) {
-                            if (min > 0 && Math.abs(array[i][j]) < Math.abs(min)) {
-                                array[i][j] *= (-1);
-                            } else if (min < array[i][j] && waterDepth < array[i][j]) {
-                                array[i][j]--;
-                            }
-                        }
-
-                    }
-                } else if ((i == coordinates[0] && j == coordinates[1]) && array[i][j] > min && min < 0) {
-                    if (array[i][j] > 0) {
-                        array[i][j] *= (-1);
-                    } else if (array[i][j] < 0) {
-
-                        array[i][j]--;
-                    }
-                }
-            }
-        }
-
-
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 System.out.print(array[i][j] + " ");
@@ -95,32 +28,89 @@ public class Main {
 
     }
 
+    public static int[][] changeTerrain(int[][] terrain, int[] coordinates) {
+        int waterDepth = 0;
+        for (int i = 0; i < terrain.length; i++) {
+            for (int j = 0; j < terrain.length; j++) {
+                if ((i <= coordinates[1] && j <= coordinates[2]) && (terrain[i][j] <= coordinates[0]) || terrain[i][j] < 0) {
+                    if (i == terrain.length - 1 && j < terrain.length - 1) {
+                        if (terrain[i][j + 1] > coordinates[0] && terrain[i][j + 1] < 0) {
+                            terrain[i][j + 1]--;
+                        } else if (terrain[i][j + 1] > 0 && Math.abs(terrain[i][j]) > terrain[i][j + 1] && terrain[i][j] < 0) {
+                            terrain[i][j + 1] *= (-1);
+                            break;
+                        } else if ((Math.abs(terrain[i][j]) < terrain[i][j + 1] || Math.abs(terrain[i][j]) < coordinates[0]) && terrain[i][j] < 0) {
+                            terrain[i][j]--;
+                        } else if (terrain[i][j] == coordinates[0] && terrain[i][j] < 0) {
+                            terrain[i][j]--;
+                        } else if (terrain[i][j] > 0 && terrain[i][j] > terrain[i][j + 1]) {
+                            terrain[i][j] *= (-1);
+                        } else if (terrain[i][j] > 0 && terrain[i][j] == Math.abs(coordinates[0])) {
+                            terrain[i][j] *= (-1);
+                        }
+                    } else if (j == terrain.length - 1 && i < terrain.length - 1) {
+                        if (terrain[i + 1][j] > coordinates[0] && terrain[i + 1][j] < 0) {
+                            terrain[i + 1][j]--;
+                        } else if (terrain[i + 1][j] > 0 && j < terrain.length - 1) {
+                            terrain[i + 1][j] *= (-1);
+                        } else if (Math.abs(terrain[i][j]) < terrain[i + 1][j]) {
+                            terrain[i][j]--;
+                        } else if (terrain[i][j] > 0 && terrain[i + 1][j] < Math.abs(coordinates[0]) && terrain[i][j] < coordinates[0]) {
+                            terrain[i][j] *= (-1);
+                        } else if (Math.abs(terrain[i][j]) < coordinates[0] && terrain[i][j] < 0) {
+                            terrain[i][j]--;
+                        } else if (terrain[i][j] == Math.abs(coordinates[0]) && terrain[i][j] > 0 && Math.abs(waterDepth) >= terrain[i][j]) {
+                            terrain[i][j] *= (-1);
+                        }
+                    } else if ((i < terrain.length - 1 && j < terrain.length - 1) || (i == terrain.length - 1 && j == terrain.length - 1)) {
 
-    public static int findMin(int[][] array) {
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][j] < min) {
-                    min = array[i][j];
+
+                        if (Math.abs(coordinates[0]) > Math.abs(waterDepth) && coordinates[0] < 0) {
+                            terrain[i][j]--;
+
+                        } else if (waterDepth != terrain[i][j] && coordinates[0] > 0 && terrain[i][j] > 0 && Math.abs(waterDepth) >= coordinates[0]) {
+                            terrain[i][j] *= (-1);
+
+                        } else if (waterDepth == terrain[i][j] && coordinates[0] > Math.abs(terrain[i][j])) {
+                            terrain[i][j]--;
+                        } else if (Math.abs(coordinates[0]) < Math.abs(waterDepth) && terrain[i][j] != waterDepth) {
+                            if (coordinates[0] > 0 && Math.abs(terrain[i][j]) < Math.abs(coordinates[0])) {
+                                terrain[i][j] *= (-1);
+                            } else if (coordinates[0] < terrain[i][j] && waterDepth < terrain[i][j]) {
+                                terrain[i][j]--;
+                            }
+                        }
+
+                    }
+                } else if ((i == coordinates[1] && j == coordinates[2]) && terrain[i][j] > coordinates[0] && coordinates[0] < 0) {
+                    if (terrain[i][j] > 0) {
+                        terrain[i][j] *= (-1);
+                    } else if (terrain[i][j] < 0) {
+
+                        terrain[i][j]--;
+                    }
                 }
             }
         }
-        return min;
+
+
+        return terrain;
     }
 
+
     public static int[] findMinWithCoordinates(int[][] array) {
-        int[] minInfoArray = new int[3];
+        int[] minAndCoordinates = new int[3];
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 if (array[i][j] < min) {
-                    minInfoArray[0] = array[i][j];
-                    minInfoArray[1] = i;
-                    minInfoArray[2] = j;
+                    minAndCoordinates[0] = array[i][j];
+                    minAndCoordinates[1] = i;
+                    minAndCoordinates[2] = j;
                 }
             }
         }
-        return minInfoArray;
+        return minAndCoordinates;
     }
 }
 
