8c6dba95db4bc0e7bb38255b90833efc8281e141
GGabi <gab.gel85@yahoo.com>
2019-09-21 12:17:32 +0200

    inside conditions 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] array = new int[][]{{-3, -3, -3, 4, 10}, {-3, -3, 4, 4, 10}, {-3, 3, 3, 3, 10}, {-2, -2, 2, 3, 10}, {10, 10, 10, 10}};
+        int[][] array = new int[][]{{-3, -3, 3, 4}, {-3, 3, 4, 4}, {3, 3, 3, 3}, {2, 1, 2, 3}};
 
 
         System.out.println("1st step");
@@ -18,92 +18,29 @@ public class Main {
         System.out.println("2nd step");
 
 
-        //((waterDepth != array[i][j + 1] || waterDepth != array[i + 1][j]) && (i != j) && (array[i][j + 1] > Math.abs(min) || array[i + 1][j] > Math.abs(min)))
 
         int waterDepth = -3;
-        int min = -2;
+        int min = 3;
+
+
+
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
+                if (Math.abs(min) > Math.abs(waterDepth) && min < 0) {
+                    array[i][j]--;
+                } else if ( Math.abs(waterDepth) == array[i][j] && min > 0) {
 
-
-                if ((Math.abs(array[i][j + 1]) == Math.abs(min) || Math.abs(array[i + 1][j]) == Math.abs(min)) && i != j) {
-
-                    if(array[i][j + 1] > 0){
-                        array[i][j + 1] *= (-1);
-                    }else if(array[i + 1][j] > 0){
-                        array[i + 1][j] *= (-1);
-                    } else if (array[i][j + 1] < Math.abs(waterDepth)) {
-                        if (array[i][j + 1] > 0) {
-                            array[i][j + 1] *= (-1);
-                        } else {
-                            array[i][j + 1]--;
-                        }
-                    } else if (array[i + 1][j] < Math.abs(waterDepth)) {
-                        if (array[i + 1][j] > 0) {
-                            array[i + 1][j] *= (-1);
-                        } else {
-                            array[i + 1][j]--;
-                        }
-                    } else if (Math.abs(waterDepth) < Math.abs(array[i][j + 1]) && array[i][j + 1] == array[i + 1][j]) {
-                        waterDepth--;
-                        array[i][j] = waterDepth;
-
-                    } else if (Math.abs(waterDepth) == Math.abs(array[i][j + 1]) && array[i][j + 1] == array[i + 1][j]) {
-                        if (array[i][j + 1] > 0) {
-                            array[i][j + 1] *= (-1);
-                            array[i + 1][j] *= (-1);
-                        }
-
-                    } else if (Math.abs(waterDepth) > Math.abs(array[i][j + 1]) && array[i][j + 1] == array[i + 1][j]) {
-                        if (array[i][j + 1] > 0) {
-                            array[i][j + 1] *= (-1);
-                            array[i + 1][j] *= (-1);
-                        } else {
-                            array[i + 1][j]--;
-                            array[i][j + 1]--;
-                        }
-
-                    } else if (array[i][j + 1] > array[i + 1][j]) {
-                        if (Math.abs(waterDepth) < Math.abs(array[i + 1][j])) {
-                            waterDepth--;
-                            array[i][j] = waterDepth;
-                        } else if (Math.abs(waterDepth) > Math.abs(array[i + 1][j])) {
-                            if (array[i + 1][j] > 0) {
-                                array[i + 1][j] *= (-1);
-                            } else {
-                                array[i + 1][j]--;
-                            }
-                        } else {
-                            array[i + 1][j] *= (-1);
-                        }
-
-                    } else if (array[i][j + 1] < array[i + 1][j]) {
-                        if (Math.abs(waterDepth) < Math.abs(array[i][j + 1])) {
-                            waterDepth--;
-                            array[i][j] = waterDepth;
-                        } else if (Math.abs(waterDepth) > Math.abs(array[i][j + 1])) {
-                            if (array[i][j + 1] > 0) {
-                                array[i][j + 1] *= (-1);
-                            }
-                        } else if (array[i][j + 1] == waterDepth) {
-                            if (array[i + 1][j] > 0) {
-                                array[i + 1][j] *= (-1);
-                            } else {
-                                array[i + 1][j]--;
-
-                            }
-                        } else if (array[i][j + 1] > 0) {
-                            array[i][j + 1] *= (-1);
-                        }
-
-                    }
+                    array[i][j] *= (-1);
+                }else if( waterDepth == array[i][j] && min > Math.abs(array[i][j])){
+                    array[i][j]--;
                 }
             }
         }
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
